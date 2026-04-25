37814ff442c31758d400257e6da9edc404c152e8
Heba Basil <106030003+hebabasil@users.noreply.github.com>
2026-04-25 16:26:11 +0300

    Implement main method in TestFizzBuzz class
    Added a main method to test FizzBuzz functionality.

--- Task1/FizzBuzz.java/TestFizzBuzz.java
+++ Task1/FizzBuzz.java/TestFizzBuzz.java
@@ -1,1 +1,22 @@
+public class TestFizzBuzz {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        FizzBuzz tester = new FizzBuzz();
+        
+        System.out.println("=== FizzBuzz Program ===");
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        
+        String resultFromFizzBuzz = tester.fizzBuzz(number);
+        String resultFromOverloadedFizzBuzz = tester.fizzBuzz(number, "Fizz", "Buzz", "FizzBuzz");
+        
+        System.out.println("Result From the basic method : " + resultFromFizzBuzz);
+        System.out.println("Result From the Overloaded method : " + resultFromOverloadedFizzBuzz);
+
+        
+      
+        
+    }
+}
 
