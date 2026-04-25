e4ce3bebe6bd148cf643479d79ecd1f41ec55a80
Heba Basil <106030003+hebabasil@users.noreply.github.com>
2026-04-25 16:43:44 +0300

    Add Item class with name and price attributes

--- Task7/OOP practice /Item.java
+++ Task7/OOP practice /Item.java
@@ -1,1 +1,31 @@
 
+package package1;
+
+public class Item {
+	private String name ;
+	private double price ;
+	
+
+	public Item(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		if (price <= 0) {
+			System.out.println("The price should be > 0");
+        }else {
+        this.price = price;
+	} 
+	
+	
+	}
+}
