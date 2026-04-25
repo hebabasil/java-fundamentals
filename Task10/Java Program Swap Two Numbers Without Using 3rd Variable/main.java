5e279119d050584f1dc327365c6a5169c461f176
Heba Basil <106030003+hebabasil@users.noreply.github.com>
2026-04-25 16:53:08 +0300

    Add Java program to swap two numbers without temp

---
+++ Task10/Java Program Swap Two Numbers Without Using 3rd Variable/main.java
@@ -0,0 +1,17 @@
+package package2;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		int a = 10;
+        int b = 20;
+
+        a = a + b;  // a = 30
+        b = a - b;  // b = 10
+        a = a - b;  // a = 20
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+	}
+
+}
